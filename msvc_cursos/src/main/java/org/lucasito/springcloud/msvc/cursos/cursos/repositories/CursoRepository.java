6c1a5c980ba4fa71f22c05f5d548e27eae82ec6d
Lucas <93831338+lucasito0208@users.noreply.github.com>
2024-04-03 11:13:55 +0200

    cambios oportunos

--- msvc_cursos/src/main/java/org/lucasito/springcloud/msvc/cursos/cursos/repositories/CursoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.lucasito.springcloud.msvc.cursos.cursos.repositories;
-
-import org.lucasito.springcloud.msvc.cursos.cursos.models.entity.Curso;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends CrudRepository<Curso, Long> {
-
-}
